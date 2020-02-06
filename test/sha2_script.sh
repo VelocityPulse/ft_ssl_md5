02810a0c0b32d1102e9a974767732f4234f33687
Velocitypulse <velocitypulse@outlook.fr>
2020-02-06 12:48:34 +0100

    [TEST] Updating sh

--- test/sha2_script.sh
+++ test/sha2_script.sh
@@ -5,7 +5,7 @@ while true
 do
 	VAR="test/a$i"
 	echo "sha2 : $i"
-	ret1=`./ft_ssl -sha2 -q $VAR`
+	ret1=`./ft_ssl -sha256 -q $VAR`
 	ret2=`shasum -a256 $VAR | grep -Eo '^[^ ]+'`
 	if [[ $ret1 != $ret2 ]] ; then
 		echo "$ret1 DIFF !!!!!!!!!!!!!!!!!!!!!!!!!!!!"
